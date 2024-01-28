7dbab6b1187148288dcdef060928cda0246e7087
MarcellVoller <88933166+MarcellVoller@users.noreply.github.com>
2024-01-28 19:56:45 +0100

    Create setup.py

---
+++ Project/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='cheat-function',
+    url='https://github.com/MarcellVoller/cheat-function',
+    author='Marcell Völler',
+    author_email='marcellvoeller@yahoo.com',
+    # Needed to actually package something
+    packages=['Project'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='A simple cheat function for PIPS',
+    # long_description=open('README.md').read(),
+)
